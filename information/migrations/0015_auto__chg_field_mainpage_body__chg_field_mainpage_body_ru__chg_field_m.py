3f930ee2d4f7adebbea217e4e2b7fddf5bc88c09
Katsiarynka <borikova.ekaterina@gmail.com>
2014-10-31 22:47:50 +0300

    main page text_field
    main page text_field

---
+++ information/migrations/0015_auto__chg_field_mainpage_body__chg_field_mainpage_body_ru__chg_field_m.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'MainPage.body'
+        db.alter_column(u'information_mainpage', 'body', self.gf('django.db.models.fields.TextField')())
+
+        # Changing field 'MainPage.body_ru'
+        db.alter_column(u'information_mainpage', 'body_ru', self.gf('django.db.models.fields.TextField')(null=True))
+
+        # Changing field 'MainPage.body_en'
+        db.alter_column(u'information_mainpage', 'body_en', self.gf('django.db.models.fields.TextField')(null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'MainPage.body'
+        db.alter_column(u'information_mainpage', 'body', self.gf('tinymce.models.HTMLField')(max_length=3000))
+
+        # Changing field 'MainPage.body_ru'
+        db.alter_column(u'information_mainpage', 'body_ru', self.gf('tinymce.models.HTMLField')(max_length=3000, null=True))
+
+        # Changing field 'MainPage.body_en'
+        db.alter_column(u'information_mainpage', 'body_en', self.gf('tinymce.models.HTMLField')(max_length=3000, null=True))
+
+    models = {
+        u'information.mainpage': {
+            'Meta': {'object_name': 'MainPage'},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'body_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'body_ru': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'related'", 'null': 'True', 'to': u"orm['information.MainPage']"}),
+            u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        u'information.news': {
+            'Meta': {'object_name': 'News'},
+            'body': ('tinymce.models.HTMLField', [], {}),
+            'body_en': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'body_ru': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'related'", 'null': 'True', 'to': u"orm['information.News']"}),
+            u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'title_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'title_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'title_ru': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        u'information.usefullinformation': {
+            'Meta': {'object_name': 'UsefullInformation'},
+            'body': ('tinymce.models.HTMLField', [], {}),
+            'body_en': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'body_ru': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'related'", 'null': 'True', 'to': u"orm['information.UsefullInformation']"}),
+            u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'title_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'title_ru': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['information']
