c6e650acbd404a5f89001660e1a2242e9aa583a2
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-02 19:35:28 +0300

    Change in contacts
    Delete function send_message - isn't using

--- contacts/views.py
+++ contacts/views.py
@@ -43,35 +43,3 @@ def contacts(request):
                           context,
                           context_instance=RequestContext(request))
 
-
-
-def send_message(request):
-    context = {}
-    return HttpResponse(json.dumps(context), content_type='application/json')
-
-
-class AjaxExampleForm(CreateView):
-    template_name = ''
-    form_class = LeaveMessageForm
-
-    def form_invalid(self, form):
-        if self.request.is_ajax():
-            to_json_response = dict()
-            to_json_response['status'] = 0
-            to_json_response['form_errors'] = form.errors
-
-            to_json_response['new_cptch_key'] = CaptchaStore.generate_key()
-            to_json_response['new_cptch_image'] = captcha_image_url(to_json_response['new_cptch_key'])
-
-            return HttpResponse(json.dumps(to_json_response), content_type='application/json')
-
-    def form_valid(self, form):
-        form.save()
-        if self.request.is_ajax():
-            to_json_response = dict()
-            to_json_response['status'] = 1
-
-            to_json_response['new_cptch_key'] = CaptchaStore.generate_key()
-            to_json_response['new_cptch_image'] = captcha_image_url(to_json_response['new_cptch_key'])
-
-            return HttpResponse(json.dumps(to_json_response), content_type='application/json')
--- seafoodservice/urls.py
+++ seafoodservice/urls.py
@@ -11,15 +11,19 @@ urlpatterns = patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^$', 'seafoodservice.views.main', name='home'),
-    url(r'^contacts$', 'contacts.views.contacts', name='contacts'),
     url(r'^summernote/', include('django_summernote.urls')),
+    url(r'^captcha/', include('captcha.urls')),
 )
 
 #services
 urlpatterns += patterns('services.views',
     url(r'^services/$', 'services', name='services'),
+    )
+
+#contacts
+urlpatterns += patterns('contacts.views',
+    url(r'^send_message/$', 'send_message', name='send_message'),
     )
 
 #news
