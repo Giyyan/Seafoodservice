ba20b7a2c957508e1ffb833e02581a83f8cb4cfe
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-03 00:45:30 +0300

    Add tag active url
    Add tag active url

--- pages/views.py
+++ pages/views.py
@@ -5,15 +5,16 @@ from django.template import RequestContext
 from partners.models import Partner
 from gallery.models import PhotoGallery
 from information.models import News, MainPage
+from information.views import set_required_data
 
 def main(request):
     photos = PhotoGallery.objects.filter(add_this_photo_to_slide=True)
     context = {
         "partners": Partner.objects.all(),
         "slides": photos,
-        "last_news": News.objects.all().order_by("-date")[:4],
         "main_page": MainPage.objects.all()[0]
     }
+    context.update(set_required_data(request))
     return render_to_response('main.html',
                           context,
                           context_instance=RequestContext(request))
