4f8d8f41773185fd575ace9d65b6c9fe55977dc9
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-03 21:58:29 +0300

    local_settings

---
+++ seafoodservice/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sfs',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
