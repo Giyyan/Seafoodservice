8e2fc4ec6f70e7d0e85593d30f8b75a715dfe752
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-03 11:49:43 +0300

    add static to urls

--- seafoodservice/urls.py
+++ seafoodservice/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^summernote/', include('django_summernote.urls')),
     url(r'^captcha/', include('captcha.urls')),
 )
