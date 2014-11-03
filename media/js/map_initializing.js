a68dd5a82a245e1d3642bd10ad386b43c18575ee
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-04 01:24:03 +0300

    google maps

--- media/js/map_initializing.js
+++ media/js/map_initializing.js
@@ -2,23 +2,25 @@ function initialize() {
     var myLatlng = new google.maps.LatLng(17.412856, 50.283015);
     var mapOptions = {
         zoom: 2,
-        center: myLatlng
+        center: myLatlng,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     var points = $('#transporation_points').children().toArray();
 
     var infoWindow = new google.maps.InfoWindow({
-        content: 'myContent',
+        content: 'myContent'
     });
     var shape = {
         coord: [1, 1, 1, 20, 18, 20, 18 , 1],
         type: 'poly'
     };
-    var image = new google.maps.MarkerImage('/media/css/images/google_map_point.png',
-        new google.maps.Size(20, 32),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(0, 32));
+    var image = new google.maps.MarkerImage('/media/css/images/google_map_point.png');
     var markers = new Array();
     var infoWindows = new Array();
     for (var i = 0; i < points.length; i++) {
@@ -30,10 +32,10 @@ function initialize() {
             icon: image
         });
         marker.info = new google.maps.InfoWindow({
-            content: '<p style="color: red; margin-bottom: -10px;">' + $('#title_' + i).text() + '</p>' + $('#desription_' + i).text()
+            content: '<p style="color: red;">' + $('#title_' + i).text()+'</p><a style="margin:3px;">' + $('#description_' + i).text()+'</a>'
         });
         google.maps.event.addListener(marker, 'click', function () {
-            marker.info.open(map, marker);
+            this.info.open(map, this);
         });
         markers.push(marker);
     }
