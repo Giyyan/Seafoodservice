b55d1724e04005e6326ea3ad6c71b33152aa819a
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-07 18:34:50 +0200

    contacts

--- media/js/map_initializing.js
+++ media/js/map_initializing.js
@@ -1,7 +1,7 @@
 function initialize() {
-    document.getElementById('map-canvas').style.height = '450px';
+    document.getElementById('map-canvas').style.height = '400px';
     document.getElementById('map-canvas').style.width = '794px';
-    document.getElementById('google_map').style.height = '450px';
+    document.getElementById('google_map').style.height = '400px';
     var myLatlng = new google.maps.LatLng(parseFloat($('#center_latitude').text()), parseFloat($('#center_longitude').text()));
     var mapOptions = {
         zoom: 2,
