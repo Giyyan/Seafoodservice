61dad6007a34ac90c6838eacfb468b086b23cf35
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-07 11:50:08 +0200

    partners

---
+++ partners/migrations/0002_auto__add_field_partner_url.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Partner.url'
+        db.add_column(u'partners_partner', 'url',
+                      self.gf('django.db.models.fields.URLField')(default=None, max_length=200),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Partner.url'
+        db.delete_column(u'partners_partner', 'url')
+
+
+    models = {
+        u'partners.partner': {
+            'Meta': {'object_name': 'Partner'},
+            'company_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logotype': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['partners']
