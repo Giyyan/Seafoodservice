b8627f1961c9a5b59029924daf81cb8cb2eff048
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-07 20:13:40 +0200

    active url

--- pages/templatetags/active_url.py
+++ pages/templatetags/active_url.py
@@ -8,9 +8,12 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def is_active_url(context, url_name):
     request = context['request']
+    print reverse(url_name)
+    print dir(request)
+    print request.get_full_path
+    print "=" * 80
     if (url_name.find('home')+1):
         return "active" if request.path is '/' else ""
     else:
         url = reverse(url_name)
         return "active" if request.path.find(url) + 1 else ""
-    
