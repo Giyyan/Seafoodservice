e6ef1c9ab88b59d40e7abed059cec1fd673e0084
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-07 18:50:25 +0300

    active_url

--- pages/templatetags/active_url.py
+++ pages/templatetags/active_url.py
@@ -1,11 +1,15 @@
 # -*- coding=utf-8 -*-
 from django import template
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
+
 @register.simple_tag
-def active(request, pattern):
-    import re
-    if re.search(pattern, request.path):
-        return 'active'
-    return ''
+def is_active_url(request, url_name):
+    if (url_name is 'home'):
+        return "active" if request.path == '/' else ""
+    else:
+        url = reverse(url_name)
+        return "active" if request.path.find(url) + 1 else ""
+    print url, request.path
