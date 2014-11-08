0732494513e9d8698b24a431be19a6a750bb951f
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-08 18:14:47 +0300

    Gallery and certificates popup

--- media/js/gallery_size.js
+++ media/js/gallery_size.js
@@ -1,4 +1,18 @@
 function set_width_to_media_files() {
+   set_width_to_images();
+   set_width_to_video_files();
+}
+
+function set_width_to_images() {
+
+    var width_of_media = parseInt(document.getElementById('photo_gallery').offsetWidth*(0.94) / 3);
+    var photos = document.getElementsByClassName('img_in_gallery');
+    for(var i=0; i<photos.length; i++){
+        photos[i].width = width_of_media;
+    }
+}
+
+function set_width_to_video_files() {
 
     var width_of_media = parseInt(document.getElementById('video_gallery').offsetWidth*(0.94) / 3);
     var  videos = document.getElementsByTagName('video');
@@ -9,5 +23,5 @@ function set_width_to_media_files() {
     for(var i=0; i<photos.length; i++){
         photos[i].width = width_of_media;
     }
-
 }
+
