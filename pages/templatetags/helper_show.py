f03015067048f424641f9634bc4ea37401cca366
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-08 18:15:50 +0300

    show block latest news

--- pages/templatetags/helper_show.py
+++ pages/templatetags/helper_show.py
@@ -1,9 +1,29 @@
 from django import template
 from django.core.urlresolvers import reverse
+from django.template import Context
+from django.utils.translation import ugettext_lazy as _
+
+from information.models import News, UsefullInformation
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def show(context, url_name, count):
-    pass
+def show(context, name, count_of_latest_news):
+    objs = []
+    title = ''
+    url = ''
+    if(name is u'news'):
+        title = _(u"News")
+        objs = News.objects.order_by('-date')[:count_of_latest_news].values_list('id','title', 'date')
+        url = reverse('news')
+    elif(name is u'uinfo'):
+        title = _(u"Useful Information")
+        objs = UsefullInformation.objects.order_by('-date')[:count_of_latest_news].values_list('id','title')
+        url = reverse('usefull_information')
+    t = template.loader.get_template('show_latest_news.html')
+
+    return t.render(Context({'objs': objs,
+                             'title': title,
+                             'url': url},
+                            autoescape=context.autoescape))
