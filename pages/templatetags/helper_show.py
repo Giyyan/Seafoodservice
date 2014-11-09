d244a197a3e0cdcf3f9bd1bf59ca728b1cae3cfe
Katsiarynka <borikova.ekaterina@gmail.com>
2014-11-09 16:46:32 +0300

    helper show

--- pages/templatetags/helper_show.py
+++ pages/templatetags/helper_show.py
@@ -8,22 +8,23 @@ from information.models import News, UsefullInformation
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def show(context, name, count_of_latest_news):
+@register.simple_tag()
+def show(name, count_of_latest_news):
     objs = []
     title = ''
-    url = ''
-    if(name is u'news'):
+    url = url_item = ''
+    print "'"+name+"'", type(name)
+    if(name == 'news'):
         title = _(u"News")
-        objs = News.objects.order_by('-date')[:count_of_latest_news].values_list('id','title', 'date')
+        objs = News.objects.order_by('-date')[:count_of_latest_news].values('id','title', 'date')
         url = reverse('news')
+        url_item = 'news_item'
     elif(name is u'uinfo'):
         title = _(u"Useful Information")
-        objs = UsefullInformation.objects.order_by('-date')[:count_of_latest_news].values_list('id','title')
+        objs = UsefullInformation.objects.order_by('-date')[:count_of_latest_news].values('id','title')
         url = reverse('usefull_information')
-    t = template.loader.get_template('show_latest_news.html')
+        url_item = 'usefull_information_item'
+    print objs, title, url
+    return {'objs': objs, 'title': title, 'url': url, 'url_item':url_item}
 
-    return t.render(Context({'objs': objs,
-                             'title': title,
-                             'url': url},
-                            autoescape=context.autoescape))
+register.inclusion_tag('show_latest_news.html')(show)
