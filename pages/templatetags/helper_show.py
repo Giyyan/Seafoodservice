9fb71dff4dbf935af56ae02b20638b1ef345979e
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-10 10:07:41 +0200

    block news

--- pages/templatetags/helper_show.py
+++ pages/templatetags/helper_show.py
@@ -1,6 +1,5 @@
 from django import template
 from django.core.urlresolvers import reverse
-from django.template import Context
 from django.utils.translation import ugettext_lazy as _
 
 from information.models import News, UsefullInformation
@@ -8,23 +7,15 @@ from information.models import News, UsefullInformation
 register = template.Library()
 
 
-@register.simple_tag()
-def show(name, count_of_latest_news):
-    objs = []
-    title = ''
-    url = url_item = ''
-    print "'"+name+"'", type(name)
-    if(name == 'news'):
+@register.inclusion_tag('show_latest_news.html')
+def show(name='news', count_of_latest_news=10):
+    url = reverse(name)
+    url_item = '{}_item'.format(name,)
+    if name == 'news':
         title = _(u"News")
-        objs = News.objects.order_by('-date')[:count_of_latest_news].values('id','title', 'date')
-        url = reverse('news')
-        url_item = 'news_item'
-    elif(name is u'uinfo'):
+        model = News
+    else:
         title = _(u"Useful Information")
-        objs = UsefullInformation.objects.order_by('-date')[:count_of_latest_news].values('id','title')
-        url = reverse('usefull_information')
-        url_item = 'usefull_information_item'
-    print objs, title, url
-    return {'objs': objs, 'title': title, 'url': url, 'url_item':url_item}
-
-register.inclusion_tag('show_latest_news.html')(show)
+        model = UsefullInformation
+    objs = model.objects.order_by('-date')[:count_of_latest_news]
+    return {'objs': objs, 'title': title, 'url': url, 'url_item': url_item}
