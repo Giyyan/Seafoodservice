a37f6bf0f4e0c2d6f45253a6260fc52bd71f3eb7
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-10 11:52:27 +0200

    block news

--- seafoodservice/urls.py
+++ seafoodservice/urls.py
@@ -22,34 +22,33 @@ urlpatterns = patterns('',
 #pages
 urlpatterns += patterns('pages.views',
     url(r'^$', 'main', name='home'),
-    )
+)
 
 #services
 urlpatterns += patterns('services.views',
     url(r'^services/$', 'services', name='services'),
-    )
+)
 
 #contacts
 urlpatterns += patterns('contacts.views',
     url(r'^contacts/$', 'contacts', name='contacts'),
-    )
+)
 
 #news
 urlpatterns += patterns('information.views',
     url(r'^news/$', 'news', name='news'),
     url(r'^news/(?P<news_id>\d+)/$', 'news_item', name='news_item'),
     url(r'^useful-information/$', 'useful_information', name='usefull_information'),
-    url(r'^useful-information/(?P<information_id>\d+)/$', 'usefull_information_item', name='useful_information_item'),
-                        )
+    url(r'^useful-information/(?P<information_id>\d+)/$', 'useful_information_item', name='usefull_information_item'),
+)
 
 #gallery
 urlpatterns += patterns('gallery.views',
     url(r'^gallery/$', 'gallery', name='gallery'),
     url(r'^get_slide_images/$', 'get_slide_images', name='get_slide_images'),
-
-    )
+)
 
 #certificates
 urlpatterns += patterns('certificates.views',
     url(r'^certificates/$', 'certificates', name='certificates'),
-                        )
+)
