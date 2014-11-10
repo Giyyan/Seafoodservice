27551c7e110326a4093fadd87ed4ba53e7f2bd89
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-10 10:49:45 +0200

    block news

--- pages/templatetags/active_url.py
+++ pages/templatetags/active_url.py
@@ -8,5 +8,9 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def is_active_url(context, url_name):
     request = context['request']
+    print reverse(url_name)
+    print request.get_full_path()
+    print 'active' \
+        if reverse(url_name) == request.get_full_path() else ''
     return 'active' \
         if reverse(url_name) == request.get_full_path() else ''
--- pages/templatetags/helper_show.py
+++ pages/templatetags/helper_show.py
@@ -1,7 +1,6 @@
 from django import template
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
-
 from information.models import News, UsefullInformation
 
 register = template.Library()
@@ -11,11 +10,11 @@ register = template.Library()
 def show(name='news', count_of_latest_news=10):
     url = reverse(name)
     url_item = '{}_item'.format(name,)
-    if name == 'news':
-        title = _(u"News")
-        model = News
-    else:
+    if name == 'usefull_information':
         title = _(u"Useful Information")
         model = UsefullInformation
+    else:
+        title = _(u"News")
+        model = News
     objs = model.objects.order_by('-date')[:count_of_latest_news]
     return {'objs': objs, 'title': title, 'url': url, 'url_item': url_item}
