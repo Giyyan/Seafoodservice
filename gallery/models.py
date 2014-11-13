7113b8dbe8de74e2a5fd10009d5d1c7afb0c9fb3
Karbanovich Andrey <karbanovich.andrey@gmail.com>
2014-11-13 08:24:00 +0200

    vk link

--- gallery/models.py
+++ gallery/models.py
@@ -5,11 +5,12 @@ from mptt.models import MPTTModel
 
 from fields import ExtFileField
 
+
 class PhotoGallery(MPTTModel):
     parent = models.ForeignKey('self', null=True, blank=True, related_name="related")
     photo = ExtFileField(upload_to='photos', verbose_name=_("Image"),
                              ext_whitelist=(".png", ".jpg", ".PNG", ".JPG", ".jpeg", ".JPEG"))
-    description= models.CharField(max_length=255, blank=True, verbose_name=_("Description"))
+    description = models.CharField(max_length=255, blank=True, verbose_name=_("Description"))
     title = models.CharField(max_length=255, blank=True, verbose_name=_("Title"))
     add_this_photo_to_slide = models.BooleanField(null=False, verbose_name=_(u"Add this photo to slide"))
     add_this_photo_to_gallery = models.BooleanField(null=False, verbose_name=_(u"Add this photo to gallery"))
